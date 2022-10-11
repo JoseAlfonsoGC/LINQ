346fa71bd9e2ecb8ed3ac12d1a95f6d35c7be72e
bfjose08 <bfjose26@gmail.com>
2022-10-11 15:27:48 -0500

    -clase que use Linq -métodos que retornen resultados de query

---
+++ 01-02-Query_desde_metodos/CclaseExplicita.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_02_Query_desde_metodos
+{
+    class CclaseExplicita
+    {
+        //arreglo sobre el cual trabajar
+        private static string[] postres = { "Pay de manzana", "Pastel de chocolate", "Manzana caramelizada", "Fresas con crema" };
+
+        //ejemplo 01 query
+        //no puede usarse de forma implicita
+        //dibe de ser estatico
+        private static IEnumerable<string> encontrados = from p in postres
+                                                         where p.Contains("manzana")
+                                                         orderby p
+                                                         select p;
+        //ejemplo02
+        //el metodo regresa y todo se trabaja internamente
+        public static IEnumerable<int> obtenerNumerosPares()
+        {
+            int[] numero = { 1, 5, 4, 7, 6, 3, 5, 9, 8, 11 };
+
+            IEnumerable<int> pares = from n in numero
+                                     where n % 2 == 0
+                                     select n;
+
+            return pares;
+        }
+        //metodo que regresa el resultado del query
+        public static IEnumerable<string> ObtenerPostres()
+        {
+            return encontrados;
+        }
+        //metodo que regresa el resultado de un query inmediato
+        public static int[] ObtenerNumerosImpares()
+        {
+            int[] numeros = { 1, 5, 4, 7, 6, 3, 5, 9, 8, 11 };
+
+            var resultado = from n in numeros
+                            where n % 2 != 0 //numeros inpares
+                            select n;
+            return resultado.ToArray();
+        }
+    }
+}
---
+++ 01-02-Query_desde_metodos/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_02_Query_desde_metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //obtener resultados del query desde metodos
+
+            //Invocamos el metodo
+            IEnumerable<int> resultados = CclaseExplicita.obtenerNumerosPares();
+
+            //Mostrar los resultados
+            foreach (int num in resultados)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("-----------------");
+
+            IEnumerable<string> postres = CclaseExplicita.ObtenerPostres();
+
+            //mostramos resultado
+            foreach (string p in postres)
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine("-----------------");
+
+            int[] impares = CclaseExplicita.ObtenerNumerosImpares();
+
+            //
+            foreach (int m in impares)
+            {
+                Console.WriteLine(m);
+            }
+            Console.ReadKey();
+        }
+    }
+}
